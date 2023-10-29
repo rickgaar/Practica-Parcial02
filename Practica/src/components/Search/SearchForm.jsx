5f7af346713321bf23cacdeb1a33b23e80bb4387
rickgaar <00148922@uca.edu.sv>
2023-10-28 21:32:40 -0600

    add: form section

---
+++ Practica/src/components/Search/SearchForm.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+const SearchForm = () => {
+    const [category, setCategory] = useState('');
+    
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+    } 
+
+    const getData = async (e) => {
+        e.preventDefault();
+        /* Funcion para el fetch */
+        let response = await fetchQuestion(category);
+
+        /* Funcion para crear la pregunta */
+    }
+    
+    return(
+        <div className="bg-violet-400 justify-center w-full p-8 items-center">
+            <form className="flex flex-col gap-8 items-center">
+                <label htmlFor="category" className="text-center">Category:</label>
+                <select id="category" onChange={handleCategoryChange} className="h-8 w-1/2">
+                    <option value={""}>Any Category</option>
+                    <option value={9}>General Knowledge</option>
+                    <option value={15}>Video Games</option>
+                    <option value={20}>Mythology</option>
+                    <option value={29}>Comics</option>
+                    <option value={12}>Music</option>
+                    <option value={19}>Mathematics</option>
+                </select>
+                <button type="submit" className="bg-violet-600 p-4 rounded-md text-white">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default SearchForm;
