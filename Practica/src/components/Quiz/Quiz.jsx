51683a984a637f7e3caf0b96e91535fa7caf735d
MarcoArana <00037822@uca.edu.sv>
2023-10-29 18:21:16 -0600

    Los cocos

--- Practica/src/components/Quiz/Quiz.jsx
+++ Practica/src/components/Quiz/Quiz.jsx
@@ -1,13 +1,58 @@
 import Controls from "./Controls/Controls";
 import Questions from "./Questions/Questions";
+import { useEffect, useState } from "react";
+import { getExamFromLS, saveExamInLS } from "../../service/kahootservice";
 
-function Quiz({QyA=[], currentQuestion={}, onPrev=()=>{}, onNext=()=>{}, position=1}){
-    return(
+function Quiz({ questionsArray = [] }) {
+
+    const [QyA, setQyA] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState([]);
+    const [position, setPosition] = useState(1);
+    const oldExams = getExamFromLS();
+
+    useEffect(
+        () => { updateCurrentQuestion(position - 1) },
+        [position]);
+
+    useEffect(
+        () => {addQyA(questionsArray)},[]
+    );
+
+    const updateCurrentQuestion = (pos) => {
+        setCurrentQuestion(QyA[pos])
+    }
+
+    const addQyA = (QyA) => {
+        console.log(questionsArray);
+        setQyA([...QyA]);
+        setPosition(1);
+        setCurrentQuestion(QyA[0]);
+        oldExams.push(QyA);
+        //console.log(oldExams);
+        saveExamInLS(oldExams);
+    }
+
+    const onPageChange = (_page = 0) => {
+
+        let _position = position + _page;
+
+        if (_position <= 0) {
+            _position = 1;
+        }
+        console.log(QyA.length);
+        if (_position > (QyA.length)) {
+            _position = QyA.length;
+        }
+
+        setPosition(_position);
+    }
+
+    return (
         <section className="flex flex-col pt-5 gap-3 items-center bg-violet-600 text-white py-8">
-            {QyA.length != 0 && <Controls onPrev={onPrev} onNext={onNext} position={position}/>}
+            {QyA.length != 0 && <Controls onPrev={() => { onPageChange(-1) }} onNext={() => { onPageChange(1) }} position={position} />}
 
-            {QyA.length != 0 && <Questions _q={currentQuestion.question} _a={[currentQuestion.correct_answer, ...currentQuestion.incorrect_answers]} key={currentQuestion.correct_answer}/>}          
-            
+            {QyA.length != 0 && <Questions _q={currentQuestion.question} _a={[currentQuestion.correct_answer, ...currentQuestion.incorrect_answers]} key={currentQuestion.correct_answer} />}
+
         </section>
     );
 }
--- Practica/src/components/Search/SearchForm.jsx
+++ Practica/src/components/Search/SearchForm.jsx
@@ -1,38 +1,44 @@
 import { useEffect, useState } from "react";
 import { fetchQuestion } from "../../service/kahootservice";
+import Quiz from "../Quiz/Quiz";
 
-const SearchForm = ({addQyA}) => {
+const SearchForm = () => {
+
+    const [questionsArray, setQuestionsArray] = useState([])
     const [category, setCategory] = useState('');
-    
+
     const handleCategoryChange = (event) => {
         setCategory(event.target.value);
-    } 
+    }
 
     const getData = async (e) => {
         e.preventDefault();
         /* Funcion para el fetch */
-        let response = await fetchQuestion({category});
-        addQyA(response);
-
-        /* Funcion para crear la pregunta */
+        let response = await fetchQuestion({ category });
+        setQuestionsArray(response);
     }
-    
-    return(
-        <div className="bg-violet-400 w-full p-8">
-            <form className="flex flex-col gap-8 items-center" onSubmit={getData}>
-                <label htmlFor="category" className="text-center">Category:</label>
-                <select id="category" onChange={handleCategoryChange} className="h-8 w-1/2">
-                    <option value={""}>Any Category</option>
-                    <option value={9}>General Knowledge</option>
-                    <option value={15}>Video Games</option>
-                    <option value={20}>Mythology</option>
-                    <option value={29}>Comics</option>
-                    <option value={12}>Music</option>
-                    <option value={19}>Mathematics</option>
-                </select>
-                <button type="submit" className="bg-violet-600 p-4 rounded-md text-white">Submit</button>
-            </form>
-        </div>
+
+    return (
+        <section>
+            <div className="bg-violet-400 w-full p-8">
+                <form className="flex flex-col gap-8 items-center" onSubmit={getData}>
+                    <label htmlFor="category" className="text-center">Category:</label>
+                    <select id="category" onChange={handleCategoryChange} className="h-8 w-1/2">
+                        <option value={""}>Any Category</option>
+                        <option value={9}>General Knowledge</option>
+                        <option value={15}>Video Games</option>
+                        <option value={20}>Mythology</option>
+                        <option value={29}>Comics</option>
+                        <option value={12}>Music</option>
+                        <option value={19}>Mathematics</option>
+                    </select>
+                    <button type="submit" className="bg-violet-600 p-4 rounded-md text-white">Submit</button>
+                </form>
+
+            </div>
+
+            <Quiz questionsArray={questionsArray}/>
+        </section>
     );
 }
 
